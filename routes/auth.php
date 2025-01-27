748a28185efd6519f78174eaf0e141995d0c80a2
Angel T <angel.t069@outlook.com>
2025-01-27 14:55:32 -0600

    Add redirection to logout and resend-code routes

--- routes/auth.php
+++ routes/auth.php
@@ -4,16 +4,28 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmailVerificationController;
 use App\Http\Controllers\AuthController;
 
-// Rutas de autenticación
-Route::get('login', [AuthController::class, 'showLoginForm'])->name('login.form');
-Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::middleware('guest')->group(function () {
+    // Rutas de autenticación
+    Route::get('login', [AuthController::class, 'showLoginForm'])->name('login.form');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
 
-// Rutas de registro
-Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register.form');
-Route::post('register', [AuthController::class, 'register'])->name('register');
+    // Rutas de registro
+    Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register.form');
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+});
 
 // Ruta de cierre de sesión
-Route::post('logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
+Route::middleware('auth')->group(function () {
+    /**
+     * La siguiente ruta GET es para evitar que la aplicación falle si se accede a '/logout' desde el navegador.
+     * La ruta POST '/logout' debe ser utilizada únicamente desde el botón que se encarga de realizar la acción de cierre de sesión.
+     * Acceder a esta ruta mediante GET redirige al usuario a la página de inicio.
+     */
+    Route::get('logout', function() {
+        return redirect()->route('home');
+    });
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
 
 // Verifica el correo electrónico cuando el usuario da clic en el enlace que se envía a su correo
 Route::get('/email/verify/{id}', [EmailVerificationController::class, 'verifyEmail'])->middleware('signed')->name('verification.verify');
--- routes/otp.php
+++ routes/otp.php
@@ -6,4 +6,13 @@ use App\Http\Controllers\OtpController;
 // Rutas de verificación OTP
 Route::get('otp/{id}', [OtpController::class, 'showOtpForm'])->middleware('logged')->whereNumber('id')->name('otp.form');
 Route::post('otp/{id}', [OtpController::class, 'verifyOtp'])->whereNumber('id')->name('otp.verify');
+
+/**
+ * La siguiente ruta GET es para evitar que la aplicación falle si se accede a '/resend-otp/{id}' desde el navegador.
+ * La ruta POST '/resend-otp/{id}' debe ser utilizada únicamente desde el formulario de verificación.
+ * Acceder a esta ruta mediante GET redirige al usuario a la página de inicio.
+ */
+Route::get('resend-otp/{id}', function() {
+    return redirect()->route('home');
+})->middleware('auth');
 Route::post('resend-otp/{id}', [OtpController::class, 'resendOtp'])->whereNumber('id')->name('otp.resend');
