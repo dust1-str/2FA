df0f81b7d3beab1ecc57ca31be9c38ba93649f3b
Angel T <angel.t069@outlook.com>
2025-01-27 20:48:45 -0600

    Document Middlewares

--- app/Http/Middleware/LoginPassed.php
+++ app/Http/Middleware/LoginPassed.php
@@ -1,34 +1,43 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
 
+/**
+ * Class LoginPassed
+ *
+ * This middleware ensures that the user has passed the Login form before accessing OTP form.
+ * Also, allows the user to try again if the OTP is incorrect.
+ *
+ * @package App\Http\Middleware
+ */
 class LoginPassed
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param  \Illuminate\Http\Request  $request The incoming request instance.
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next The next middleware to call.
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
-        // Verificar si la solicitud proviene de la ruta 'login'
+        // Check if the request comes from the 'login' route
         $referer = $request->headers->get('referer');
         
         if ($referer && strpos($referer, route('login')) !== false) {
             return $next($request);
         }
 
+        // Check if the 'otp_passed' session variable is false
         if ($request->session()->get('otp_passed') === false) {
+            // Clear the 'otp_passed' session variable
             $request->session()->forget('otp_passed');
             return $next($request);
         }
 
-        // Redirigir si no proviene de la ruta 'login'
+        // Redirect to the login form if the request does not come from the 'login' route or the 'otp_passed' session variable is not false
         return redirect()->route('login');
     }
 }
